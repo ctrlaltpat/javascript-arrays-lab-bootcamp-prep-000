11f1e17081e8b127fc9cea23e41c2382edf5a29a
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 10:11:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,10 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = kittens.
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens
 }
